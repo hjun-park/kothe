71c25a5b02bc76d64b389755f3df91b4bdc29ace
지환 <57333984+kimjihw@users.noreply.github.com>
2023-05-30 21:39:49 +0900

    [2023-05-30] programmers_수 조작하기2

---
+++ [2023-06]/[1주차]/hwan/[2023-05-30] programmers_수 조작하기2.py
@@ -0,0 +1,16 @@
+def solution(numLog):
+    answer = ''
+    lst = []
+    for i in range(1, len(numLog)):
+        lst.append(numLog[i] - numLog[i - 1])
+
+    for i in lst:
+        if i == 1:
+            answer += "w"
+        elif i == -1:
+            answer += "s"
+        elif i == 10:
+            answer += "d"
+        elif i == -10:
+            answer += "a"
+    return answer

3fa21a216d70edb7fe3065ac07dcbfb3d8c57a97
지환 <57333984+kimjihw@users.noreply.github.com>
2023-06-17 10:00:34 +0900

    [2023-06-17] programmers_삼각형의 완성조건(2)

---
+++ [2023-06]/[2주차]/jihwan/[2023-06-17] programmers_삼각형의 완성조건(2).py
@@ -0,0 +1,9 @@
+def solution(sides):
+    answer = 0
+    sides.sort()
+    for i in range(0, sides[1] + 1):
+        if sides[0] + i > sides[1]:
+            answer += 1
+    answer += sum(sides) - 1 - sides[1]
+
+    return answer

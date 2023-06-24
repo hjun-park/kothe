10af3d53c635300ca1dc6a6715b556c9d31baadf
지환 <57333984+kimjihw@users.noreply.github.com>
2023-06-24 11:23:41 +0900

    [2023-06-24] programmers_조건에 맞게 수열 변환하기2

---
+++ [2023-06]/[4주차]/jihwan/조건에 맞게 수열 변환하기 2.py
@@ -0,0 +1,16 @@
+def solution(arr):
+    answer = 0
+    lst = []
+    while True:
+        if lst == arr:
+            break
+        else:
+            lst = arr.copy()
+            for i in range(len(arr)):
+                if arr[i] >= 50 and arr[i] % 2 == 0:
+                    arr[i] = int(arr[i] / 2)
+                elif arr[i] < 50 and arr[i] % 2 == 1:
+                    arr[i] = arr[i] * 2 + 1
+            answer += 1
+
+    return answer - 1

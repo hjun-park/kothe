3114affa225adf727d309b9a212cbac99111165d
지환 <57333984+kimjihw@users.noreply.github.com>
2023-06-27 21:59:59 +0900

    [2023-06-27] programmers_배열의 길이를 2의 거듭제곱으로 만들기

---
+++ [2023-06]/[5주차]/jihwan/[2023-06-27] programmers_배열의 길이를 2의 거듭제곱으로 만들기.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+    n = 0
+    for i in range(len(arr)):
+        if len(arr) <= 2 ** i:
+            n = i
+            arr.extend([0] * (2 ** n - len(arr)))
+            break
+
+    answer = arr
+
+    return answer

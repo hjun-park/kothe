b036fa352fd78def270bae84001fd70ea3b03ca6
지환 <57333984+kimjihw@users.noreply.github.com>
2023-06-27 22:11:10 +0900

    [2023-06-27] programmers_정사각형으로 만들기

---
+++ [2023-06]/[5주차]/jihwan/[2023-06-27] programmers_정사각형으로 만들기.py
@@ -0,0 +1,11 @@
+def solution(arr):
+    answer = [[]]
+    for i in range(len(arr)):
+        if len(arr[i]) < len(arr):
+            arr[i].extend([0] * (len(arr)-len(arr[i])))
+    if len(arr[0]) > len(arr):
+        for i in range(len(arr[0]) - len(arr)):
+            arr.append([0] * len(arr[0]))
+
+    answer = arr
+    return answer
